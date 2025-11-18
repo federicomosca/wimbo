069dd7ff64a3c0dfcb9005b33e529f9a63e10be2
federicomosca <federicomosca@pm.me>
2025-11-18 23:54:18 +0100

    add filterChain

--- src/main/java/me/askew/wimbo/config/SecurityConfig.java
+++ src/main/java/me/askew/wimbo/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package me.askew.wimbo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +20,16 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web -> web.ignoring()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/", "/v3/api-docs/").permitAll()
+                        .anyRequest().permitAll() // or authenticated(), your choice
+                );
+        return http.build();
     }
 }
